abec2a9d0a3f1d4d7e1bce4b258e583d20b0689b
Kim Marquardt <kim.marquardt@gmail.com>
2014-10-23 20:14:20 -0700

    Restructure file tree

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // This file starts our server, we defer to localhost:8000
 // if a process port and url are not defined.
 
-var app = require('./server/server.js');
+var app = require('./server/server-config.js');
 
 var port = process.env.PORT || 8000;
 var url = process.env.URL || 'localhost';
 
 app.listen(port, url);
 
-// NODE_ENV is 'development by 'default'
+// NODE_ENV is 'development' by default
 (process.env.NODE_ENV === 'development') && console.log('Started Listening on', url, ':', port, 'at', Date().slice(16,24));
