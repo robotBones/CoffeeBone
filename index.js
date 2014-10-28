0b6f0f85d1f6ae55dab44c5284e2a7ea244850bf
Kim Marquardt <kim.marquardt@gmail.com>
2014-10-27 21:28:11 -0700

    Add APP_ROOT to global namespace

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 // This file is a hook to start the server
 // server files are separated from this so they're portable and testable
 var path = require('path');
-global.appRoot = path.resolve(__dirname);
+global.APP_ROOT = path.resolve(__dirname);
 
-var server = require(appRoot +'/server/server-config.js');
+var server = require(APP_ROOT +'/server/server-config.js');
 // we defer to localhost:8000 if a process port and url are not defined.
 var port = process.env.PORT || 8000;
 var url = process.env.URL || 'localhost';
