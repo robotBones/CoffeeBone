69775f8ed6e9b83cb90861fd9561300970951553
Kim Marquardt <kim.marquardt@gmail.com>
2014-10-29 12:52:54 -0700

    Remove unused files

--- server/router/index/indexRoutes.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// exported to /server/router/router.js
-module.exports = function(router){
-  // welcome page
-  router.get('/', function (req,res){
-    res.send('indexRouter says Welcome');
-  });
-};
